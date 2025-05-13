a890db07e676e16bf5dccf1463f7123bcdd23f78
seoyoung <tjdud111160@gmail.com>
2025-05-13 14:34:28 +0900

    8장: 장바구니 완료

--- src/main/java/com/shop/entity/Cart.java
+++ src/main/java/com/shop/entity/Cart.java
@@ -6,19 +6,26 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table(name="cart")
-@Getter @Setter
+@Table(name = "cart")
+@Getter
+@Setter
 @ToString
-public class Cart
+public class Cart extends BaseEntity
 {
 
     @Id
-    @Column(name="cart_id")
+    @Column(name = "cart_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="member_id")
+    @JoinColumn(name = "member_id")
     private Member member;
+
+    public static Cart createCart(Member member){
+        Cart cart = new Cart();
+        cart.setMember(member);
+        return cart;
+    }
 
 }
